e5014d0edb3429735daef0839c5f57c6dfd06fe0
josephineabioye <josephineabioye@yahoo.com>
2026-01-26 21:25:43 +0000

    update to AutoDebit class

--- src/savr/model/AutoDebit.java
+++ src/savr/model/AutoDebit.java
@@ -1,4 +1,19 @@
 package savr.model;
 
 public class AutoDebit {
+    private String pocketName;
+    private double amount;
+
+    public AutoDebit(String pName, double amt) {
+        pocketName = pName;
+        amount = amt;
+    }
+
+    public String autoDebitPocketName() {
+        return pocketName;
+    }
+
+    public double autoDebitAmount() {
+        return amount;
+    }
 }
