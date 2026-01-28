e8e7190d61cb4a3283a0c960143a082c60e09d58
josephineabioye <josephineabioye@yahoo.com>
2026-01-28 21:40:57 +0000

    update AutoDebit class

--- src/savr/model/AutoDebit.java
+++ src/savr/model/AutoDebit.java
@@ -40,4 +40,8 @@ public class AutoDebit {
     public boolean isDue() {
         return !LocalDate.now().isBefore(nextRun);
     }
+    public void markExecuted() {
+        lastRun = LocalDate.now();
+        nextRun = calculateNextRun(lastRun);
+    }
 }
