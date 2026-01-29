e189f665f65ab8c2bbafb6041d6e8fe59471b360
josephineabioye <josephineabioye@yahoo.com>
2026-01-29 23:04:59 +0000

    update AutoDebitService class

--- src/savr/service/AutoDebitService.java
+++ src/savr/service/AutoDebitService.java
@@ -1,4 +1,23 @@
 package savr.service;
 
+import savr.model.*;
+import java.io.*;
+import java.time.LocalDate;
+import java.util.*;
+
 public class AutoDebitService {
+    private List<AutoDebit> autoDebits = new ArrayList<>();
+
+    public void addAutoDebit(AutoDebit autoDebit) {
+        autoDebits.add(autoDebit);
+    }
+
+    public List<AutoDebit> getAllAutoDebits() {
+        return autoDebits;
+    }
+
+
+
+
+
 }
